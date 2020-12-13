d54b6c0926530b46a2c5afb924d5de47af863610
MEianelli <filipelouzano@gmail.com>
2020-12-12 21:53:00 -0300

    Melhorei o jogo

--- script.js
+++ script.js
@@ -2,23 +2,30 @@
 
 //window.innerWidth
 
-var wrap = document.querySelector(".wraper");
+var wrap = document.querySelector('.wraper');
+var timer = 1000;
 
 function criarNovaCaixa() {
   let randomX = Math.floor(Math.random() * 1150);
   let randomY = Math.floor(Math.random() * 550);
 
-  let novo = document.createElement("div");
-  novo.classList.add("bloco");
-  novo.innerHTML = "CLICK";
-  novo.addEventListener("click", removeBloco);
+  let novo = document.createElement('div');
+  novo.classList.add('bloco');
+  novo.innerHTML = 'CLICK';
+  novo.addEventListener('click', removeBloco);
 
-  novo.style.top = randomY + "px";
-  novo.style.right = randomX + "px";
+  novo.style.top = randomY + 'px';
+  novo.style.right = randomX + 'px';
 
   wrap.appendChild(novo);
+  timer -= 10;
 
-  setTimeout(criarNovaCaixa, 1000);
+  if (wrap.childNodes.length > 20) {
+    alert('YOU GET NOTHING!! YOU LOSE!! GOOD DAY SR!!');
+    return;
+  }
+
+  setTimeout(criarNovaCaixa, timer);
 }
 
 function removeBloco(evt) {
